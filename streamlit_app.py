79952e98cb162fe7d6dcee6b65838b10b7258e19
Gokul Prasad <gokul@Gokuls-Air.attlocal.net>
2022-04-28 21:55:51 -0700

    add

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
 import pandas as pd
 
-df = pd.read_csv("coord_events.csv")
-df.head()
+df = pd.read_csv("https://media.githubusercontent.com/media/gprasad125/soccerDemo/main/coord_events.csv")
+print(df.head())
