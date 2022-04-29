304e747d8cab3aed80a338683345316696db6b17
Gokul Prasad <gokul@Gokuls-Air.attlocal.net>
2022-04-28 21:58:30 -0700

    /

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,4 @@ import streamlit as st
 import pandas as pd
 
 df = pd.read_csv("https://media.githubusercontent.com/media/gprasad125/soccerDemo/main/coord_events.csv")
-print(df.head())
+st.write(df.head())
