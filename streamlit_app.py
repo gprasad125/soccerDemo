4a104de123e2391f26b56430c47b27a2b6f065a5
Gokul Prasad <gokul@Gokuls-Air.attlocal.net>
2022-04-28 22:13:01 -0700

    /

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import random
 
 st.markdown(
     "# Soccer Events by Latitude, Longitude Coordinates"
