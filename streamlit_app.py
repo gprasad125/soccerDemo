a3416fc4f90f79339059c35b153435a98d421d6c
Gokul Prasad <gokul@Gokuls-Air.attlocal.net>
2022-04-28 20:52:01 -0700

    added streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,8 @@
+import streamlit as st
+import pandas as pd
+import zipfile36 as zipfile
 
+path = zipfile.ZipFile("coord_events.csv.zip", "r")
+df = pd.read_csv(path.open("coord_events.csv"))
+
+df.head()
