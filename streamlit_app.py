6bc8f1243072ba3842fb6408e6f0ba0a3862cf30
Gokul Prasad <gokul@Gokuls-Air.attlocal.net>
2022-04-28 22:03:15 -0700

    /

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,10 @@
 import streamlit as st
 import pandas as pd
 
+st.markdown(
+    "# Soccer Events by Latitude, Longitude Coordinates"
+)
+
 df = pd.read_csv("https://media.githubusercontent.com/media/gprasad125/soccerDemo/main/coord_events.csv")
 st.write(df.head())
+
