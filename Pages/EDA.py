a05e87efba45336dead252f06dca25091d1ee591
Gokul Prasad <gokul@Gokuls-MacBook-Air.local>
2022-05-03 15:05:39 -0700

    editing EDA

--- Pages/EDA.py
+++ Pages/EDA.py
@@ -5,74 +5,87 @@ import matplotlib.pyplot as plt
 import random
 import altair as alt
 
-st.markdown("# Exploratory Data Analysis")
+
+def page():
+    st.markdown("# Team Event By Coordinates")
 
 
-st.markdown("## The Dataset")
-st.markdown("Here, we have our data that we used to explore.")
-coord_df = pd.read_csv("https://media.githubusercontent.com/media/gprasad125/soccerDemo/main/Data/coord_events.csv")
-def to_int(coord):
+    st.markdown("## The Dataset")
+    st.markdown("This pared-down dataset contains only four features – team name, event name, and (x, y) coordinates along the following coordinate system, where the leftmost corner represents (0, 0) and the top right represents (120, 80).")
 
-    if not pd.isnull(coord):
+    @st.cache(suppress_st_warning=True)
+    def load_and_clean():
+        coord_df = pd.read_csv("https://media.githubusercontent.com/media/gprasad125/soccerDemo/main/Data/coord_events.csv")
+        def to_int(coord):
 
-        coord = int(coord)
+            if not pd.isnull(coord):
 
-    return coord
+                coord = int(coord)
 
-coord_df['location_x'] = coord_df['location_x'].apply(to_int)
-coord_df['location_y'] = coord_df['location_y'].apply(to_int)
+            return coord
 
-clean = coord_df.dropna().reset_index(drop = True)
-clean = clean.rename(columns = {"possession_team_name": "team",
-                                "type_name": "event",
-                               "location_x": "x",
-                               "location_y": "y"})
+        coord_df['location_x'] = coord_df['location_x'].apply(to_int)
+        coord_df['location_y'] = coord_df['location_y'].apply(to_int)
 
-clean = clean.sample(frac=1).reset_index(drop = True)
-st.write(clean.head())
+        clean = coord_df.dropna().reset_index(drop = True)
+        clean = clean.rename(columns = {"possession_team_name": "team",
+                                        "type_name": "event",
+                                       "location_x": "x",
+                                       "location_y": "y"})
 
-def make_color():
+        clean = clean.sample(frac=1).reset_index(drop = True)
+        st.write("Running for 1st time only!")
+        return clean
 
-    color = "#"+''.join([random.choice('0123456789ABCDEF') for j in range(6)])
-    return color
+    data = load_and_clean()
+    st.write(data.head())
 
-event_colors = dict.fromkeys(clean['event'].unique())
-for ev in event_colors:
+    @st.cache(suppress_st_warning=True)
+    def make_color_dict():
 
-    number_of_colors = 8
-    color = make_color()
-    if color not in event_colors.values():
-        event_colors[ev] = color
+        def make_color():
+            color = "#"+''.join([random.choice('0123456789ABCDEF') for j in range(6)])
+            return color
 
-st.image('coordinates.png')
 
-madrid = clean[clean["team"] == "Real Madrid"]
+        event_colors = dict.fromkeys(data['event'].unique())
+        for ev in event_colors:
 
-fig_madrid, ax_madrid = plt.subplots()
-scatter_x = madrid['x'].to_numpy()
-scatter_y = madrid['y'].to_numpy()
-labels = madrid['event'].to_numpy()
-for l in np.unique(labels):
-    i = np.where(labels == l)
-    color = event_colors[l]
-    ax_madrid.scatter(scatter_x[i], scatter_y[i], c = color, label = l)
-ax_madrid.legend(bbox_to_anchor=(1.01, 1), loc='upper left')
-st.pyplot(fig_madrid)
+            number_of_colors = 8
+            color = make_color()
+            if color not in event_colors.values():
+                event_colors[ev] = color
 
-man_city = clean[clean["team"] == "Manchester City"]
+        return event_colors
 
-fig_city, ax_city = plt.subplots()
-scatter_x = man_city['x'].to_numpy()
-scatter_y = man_city['y'].to_numpy()
-labels = man_city['event'].to_numpy()
-for l in np.unique(labels):
-    i = np.where(labels == l)
-    color = event_colors[l]
-    ax_city.scatter(scatter_x[i], scatter_y[i], c = color, label = l)
-ax_city.legend(bbox_to_anchor=(1.01, 1), loc='upper left')
-st.pyplot(fig_city)
+    event_colors = make_color_dict()
 
-chart = alt.Chart(man_city).mark_point().encode(
-    x = "x", y = "y", color = "event").interactive().properties(
-        width = 650, height = 500, title = "Man City Event by Coord.")
-st.altair_chart(chart)
+    st.image('Assets/coordinates.png')
+
+    example_teams = ["Barcelona", "Real Madrid", "Liverpool", "Manchester United", "Manchester City"]
+    option = st.selectbox("Select a team", example_teams)
+    st.write("You have chosen: " + option)
+    #
+    option_df = data[data["team"] == option]
+    st.write(option_df.head())
+
+    fig, ax = plt.subplots()
+
+    x = option_df["x"].to_numpy()
+    y = option_df["y"].to_numpy()
+    labels = option_df["event"].to_numpy()
+
+    for l in np.unique(labels):
+        i = np.where(labels == l)
+        color = event_colors[l]
+        ax.scatter(x[i], y[i], c = color, label = l)
+    ax.legend(bbox_to_anchor=(1.01, 1), loc='upper left')
+    plt.title(option + " Events By (x, y) Coordinates")
+    st.pyplot(fig)
+
+
+    #
+    # chart = alt.Chart(option_df).mark_point().encode(
+    #     x = "x", y = "y", color = "event").interactive().properties(
+    #         width = 650, height = 500, title = option + " Event by Coord.")
+    # st.altair_chart(chart)
--- streamlit_app.py
+++ streamlit_app.py
@@ -3,27 +3,16 @@ import streamlit as st
 import numpy as np
 
 # Custom imports
-from multipage import MultiPage
-from pages import data_upload, machine_learning, metadata, data_visualize, redundant # import your pages here
+from Pages import multiPage, EDA
 
 # Create an instance of the app
-app = MultiPage()
+app = multiPage.MultiPage()
 
 # Title of the main page
-display = Image.open('Logo.png')
-display = np.array(display)
-# st.image(display, width = 400)
-# st.title("Data Storyteller Application")
-col1, col2 = st.beta_columns(2)
-col1.image(display, width = 400)
-col2.title("Data Storyteller Application")
+st.markdown("# DS3 Soccer Project")
 
 # Add all your application here
-app.add_page("Upload Data", data_upload.app)
-app.add_page("Change Metadata", metadata.app)
-app.add_page("Machine Learning", machine_learning.app)
-app.add_page("Data Analysis",data_visualize.app)
-app.add_page("Y-Parameter Optimization",redundant.app)
+app.add_page("Exploratory Data Analysis", EDA.page)
 
 # The main app
 app.run()
