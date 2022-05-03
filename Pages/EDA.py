4d25dd18f48e0f692e86154d2847e767a4d2269a
Gokul Prasad <gokul@Gokuls-MacBook-Air.local>
2022-05-03 13:47:56 -0700

    organized

---
+++ Pages/EDA.py
@@ -0,0 +1,75 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import random
+import altair as alt
+
+st.markdown("# Soccer Events by Latitude, Longitude Coordinates")
+
+coord_df = pd.read_csv("https://media.githubusercontent.com/media/gprasad125/soccerDemo/main/coord_events.csv")
+def to_int(coord):
+
+    if not pd.isnull(coord):
+
+        coord = int(coord)
+
+    return coord
+
+coord_df['location_x'] = coord_df['location_x'].apply(to_int)
+coord_df['location_y'] = coord_df['location_y'].apply(to_int)
+
+clean = coord_df.dropna().reset_index(drop = True)
+clean = clean.rename(columns = {"possession_team_name": "team",
+                                "type_name": "event",
+                               "location_x": "x",
+                               "location_y": "y"})
+
+clean = clean.sample(frac=1).reset_index(drop = True)
+st.write(clean.head())
+
+def make_color():
+
+    color = "#"+''.join([random.choice('0123456789ABCDEF') for j in range(6)])
+    return color
+
+event_colors = dict.fromkeys(clean['event'].unique())
+for ev in event_colors:
+
+    number_of_colors = 8
+    color = make_color()
+    if color not in event_colors.values():
+        event_colors[ev] = color
+
+st.image('coordinates.png')
+
+madrid = clean[clean["team"] == "Real Madrid"]
+
+fig_madrid, ax_madrid = plt.subplots()
+scatter_x = madrid['x'].to_numpy()
+scatter_y = madrid['y'].to_numpy()
+labels = madrid['event'].to_numpy()
+for l in np.unique(labels):
+    i = np.where(labels == l)
+    color = event_colors[l]
+    ax_madrid.scatter(scatter_x[i], scatter_y[i], c = color, label = l)
+ax_madrid.legend(bbox_to_anchor=(1.01, 1), loc='upper left')
+st.pyplot(fig_madrid)
+
+man_city = clean[clean["team"] == "Manchester City"]
+
+fig_city, ax_city = plt.subplots()
+scatter_x = man_city['x'].to_numpy()
+scatter_y = man_city['y'].to_numpy()
+labels = man_city['event'].to_numpy()
+for l in np.unique(labels):
+    i = np.where(labels == l)
+    color = event_colors[l]
+    ax_city.scatter(scatter_x[i], scatter_y[i], c = color, label = l)
+ax_city.legend(bbox_to_anchor=(1.01, 1), loc='upper left')
+st.pyplot(fig_city)
+
+chart = alt.Chart(man_city).mark_point().encode(
+    x = "x", y = "y", color = "event").interactive().properties(
+        width = 650, height = 500, title = "Man City Event by Coord.")
+st.altair_chart(chart)
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,77 +1,29 @@
+import os
 import streamlit as st
 import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import random
-import altair as alt
 
-st.markdown(
-    "# Soccer Events by Latitude, Longitude Coordinates"
-)
+# Custom imports
+from multipage import MultiPage
+from pages import data_upload, machine_learning, metadata, data_visualize, redundant # import your pages here
 
-df = pd.read_csv("https://media.githubusercontent.com/media/gprasad125/soccerDemo/main/coord_events.csv")
-def to_int(coord):
+# Create an instance of the app
+app = MultiPage()
 
-    if not pd.isnull(coord):
+# Title of the main page
+display = Image.open('Logo.png')
+display = np.array(display)
+# st.image(display, width = 400)
+# st.title("Data Storyteller Application")
+col1, col2 = st.beta_columns(2)
+col1.image(display, width = 400)
+col2.title("Data Storyteller Application")
 
-        coord = int(coord)
+# Add all your application here
+app.add_page("Upload Data", data_upload.app)
+app.add_page("Change Metadata", metadata.app)
+app.add_page("Machine Learning", machine_learning.app)
+app.add_page("Data Analysis",data_visualize.app)
+app.add_page("Y-Parameter Optimization",redundant.app)
 
-    return coord
-
-df['location_x'] = df['location_x'].apply(to_int)
-df['location_y'] = df['location_y'].apply(to_int)
-
-clean = df.dropna().reset_index(drop = True)
-clean = clean.rename(columns = {"possession_team_name": "team",
-                                "type_name": "event",
-                               "location_x": "x",
-                               "location_y": "y"})
-
-clean = clean.sample(frac=1).reset_index(drop = True)
-st.write(clean.head())
-
-def make_color():
-
-    color = "#"+''.join([random.choice('0123456789ABCDEF') for j in range(6)])
-    return color
-
-event_colors = dict.fromkeys(clean['event'].unique())
-for ev in event_colors:
-
-    number_of_colors = 8
-    color = make_color()
-    if color not in event_colors.values():
-        event_colors[ev] = color
-
-st.image('coordinates.png')
-
-madrid = clean[clean["team"] == "Real Madrid"]
-
-fig_madrid, ax_madrid = plt.subplots()
-scatter_x = madrid['x'].to_numpy()
-scatter_y = madrid['y'].to_numpy()
-labels = madrid['event'].to_numpy()
-for l in np.unique(labels):
-    i = np.where(labels == l)
-    color = event_colors[l]
-    ax_madrid.scatter(scatter_x[i], scatter_y[i], c = color, label = l)
-ax_madrid.legend(bbox_to_anchor=(1.01, 1), loc='upper left')
-st.pyplot(fig_madrid)
-
-man_city = clean[clean["team"] == "Manchester City"]
-
-fig_city, ax_city = plt.subplots()
-scatter_x = man_city['x'].to_numpy()
-scatter_y = man_city['y'].to_numpy()
-labels = man_city['event'].to_numpy()
-for l in np.unique(labels):
-    i = np.where(labels == l)
-    color = event_colors[l]
-    ax_city.scatter(scatter_x[i], scatter_y[i], c = color, label = l)
-ax_city.legend(bbox_to_anchor=(1.01, 1), loc='upper left')
-st.pyplot(fig_city)
-
-chart = alt.Chart(man_city).mark_point().encode(
-    x = "x", y = "y", color = "event").interactive().properties(
-        width = 650, height = 500, title = "Man City Event by Coord.")
-st.altair_chart(chart)
+# The main app
+app.run()
