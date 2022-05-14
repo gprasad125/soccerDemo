bfd61f8475c83b0ad51fc1d409bdfb8105e302be
ESR76 <esperanzasofia@gmail.com>
2022-05-13 21:20:21 -0700

    Big change to EDA

--- Pages/EDA.py
+++ Pages/EDA.py
@@ -55,7 +55,7 @@ def page():
 
         return event_colors
 
-    event_colors = make_color_dict()
+    ## INITIAL DATA SET UP AND CHOOSING TEAM #
     teams_leagues = pd.read_csv("https://media.githubusercontent.com/media/gprasad125/soccerDemo/main/Data/teams_leagues.csv")
 
     st.markdown("### Selecting a Team")
@@ -67,11 +67,11 @@ def page():
     elif (option == "NWSL"):
         st.info("Our data for **" + option + "** teams includes data from the 2018 season.")
     elif (option == "La Liga"):
-        st.info("Our data for **" + option + "** teams includes data from the 2004-2018 and 2019-2021 seasons, as well as any time a team appeared in the **Champions League** for the 2003-2005, 2006, & 2008-2019 seasons.")
+        st.info("Our data for **" + option + "** teams includes data from the 2004-2018 and 2019-2021 seasons, as well as time(s) a team appeared in the **Champions League** for the 2003-2005, 2006, & 2008-2019 seasons.")
     elif (option == "Champions League"):
         st.info("Our data for **" + option + "** teams includes data from the 2003-2005, 2006, & 2008-2019 seasons.")
     elif (option == "Premier League"):
-        st.info("Our data for **" + option + "** teams includes data from the 2003/2004 season, as well as any time a team appeared in the **Champions League** for the 2003-2005, 2006, & 2008-2019 seasons.")
+        st.info("Our data for **" + option + "** teams includes data from the 2003/2004 season, as well as time(s) a team appeared in the **Champions League** for the 2003-2005, 2006, & 2008-2019 seasons.")
     elif (option == "Men's International"):
         st.info("Our data for **" + option + "** competitions includes data from the 2018 World Cup and the 2020 UEFA Euro Cup.")
     elif (option == "Women's International"):
@@ -86,9 +86,9 @@ def page():
     num_games = num_games_df.loc[team_option]['count']
     st.info('We have ' + str(num_team_events) + ' events for **' + team_option + '** in ' + str(num_games) + ' games.')
 
+    ## EVENTS ##
     st.markdown("### Plotting Team Events By Location")
 
-    #st.markdown("### The Dataset")
     st.markdown("This reduced version of our dataset contains only four features – team name, (x, y) coordinates, and the event type.")
     st.markdown("The coordinates follow StatsBomb's coordinate system, where the top-left corner represents (0, 0) and the bottom-right represents (120, 80).")
     #st.write(data.head())
@@ -98,8 +98,6 @@ def page():
     st.write("Here are the first 5 events with location data for **" + team_option + "**.")
     st.write(option_df.head())
 
-    # fig, ax = plt.subplots()
-
     pitch = Pitch(pitch_type = 'statsbomb', positional = True, shade_middle = True)
     # specifying figure size (width, height)
     fig, ax = pitch.draw(figsize=(16, 8))
@@ -108,24 +106,27 @@ def page():
     y = option_df["y"].to_numpy()
     labels = option_df["event"].to_numpy()
     unique_labels = np.unique(labels)
+    event_colors = make_color_dict()
 
-    event_options = st.multiselect("Please choose which events you would like to see plotted.", unique_labels, default = ['Shot', 'Goal Keeper'])
+    event_form = st.form("events")
+    event_options = event_form.multiselect("Please choose which events you would like to see plotted.", unique_labels, default = ['Shot', 'Goal Keeper'])
+    event_submitted = event_form.form_submit_button("Once you're done choosing your events, click this button.")
 
-    for l in event_options:
-        i = np.where(labels == l)
-        color = event_colors[l]
-        ax.scatter(x[i], y[i], c = color, label = l)
-    ax.legend(bbox_to_anchor=(1.01, 1), loc='upper left')
-    plt.title(team_option + " Events by (x, y) Coordinates")
+    if event_submitted and len(event_options) != 0:
+        for l in event_options:
+            i = np.where(labels == l)
+            color = event_colors[l]
+            ax.scatter(x[i], y[i], c = color, label = l, s = 1)
+        ax.legend(bbox_to_anchor=(1.01, 1), loc='upper left')
+        plt.title(team_option + " Events by (x, y) Coordinates")
 
-    st.pyplot(fig, ax)
+        st.pyplot(fig, ax)
+    elif len(event_options) == 0:
+        st.markdown('You have not selected any events - please select again.')
 
     # RETURN TO TEAM SELECITON
     team_select = 'selecting-a-team'
-    st.markdown(f"<a href='#{team_select}'>Try a different a team?</a>", unsafe_allow_html=True)
-
-    #ax.text('Defending', (5, 5), xycoords = 'figure points')
-    #plt.text('Attacking')
+    st.markdown(f"<a href='#{team_select}'>Try a different team?</a>", unsafe_allow_html=True)
 
     """
     Old Version - haven't deleted yet in case I still need it:
@@ -164,6 +165,7 @@ def page():
         st.pyplot(fig)
     """
 
+    ## SHOT BAR GRAPHS ##
     st.markdown("### Analyzing a Team's Shots")
     shot_data = pd.read_csv("https://media.githubusercontent.com/media/gprasad125/soccerDemo/main/Data/shots_reduced.csv")
     shot_data = shot_data.rename(columns = {'team_name': 'team', 'play_pattern_name': 'play_pattern'})
@@ -200,15 +202,95 @@ def page():
     shotcol2.pyplot(fig3, axs3)
 
     # RETURN TO TEAM SELECTION
-    st.markdown(f"<a href='#{team_select}'>Try a different a team?</a>", unsafe_allow_html=True)
+    st.markdown(f"<a href='#{team_select}'>Try a different team?</a>", unsafe_allow_html=True)
 
-    st.markdown("### Exploring Team Lineup Frequencies")
+    ## PLAYER ANALYSIS SECTION ##
+    st.markdown("### Design Your Own Team")
+    st.markdown("Design your own version of **" + team_option + "**. You must choose exactly 11 positions (1 goalkeeper, and 10 of your choice). This will show the average pass of each of the 11 players in the positions you put them in (average pass is determined by starting location, length, and angle).")
+    
+    ppa_data = pd.read_csv("https://media.githubusercontent.com/media/gprasad125/soccerDemo/main/Data/player_pos_averages.csv").set_index(['team_name','position_name','player_name'])
+    team_averages = ppa_data.loc[team_option]
 
-    num_lineups = 5
+    attack = ['Left Wing', 'Right Center Forward', 'Right Wing', 'Striker', 'Left Center Forward', 'Secondary Striker']
+    mid = ['Right Defensive Midfield', 'Center Defensive Midfield', 'Left Defensive Midfield', 'Right Midfield', 'Right Center Midfield', 'Center Midfield', 'Left Center Midfield', 'Left Midfield', 'Right Attacking Midfield', 'Center Attacking Midfield', 'Left Attacking Midfield']
+    defense = ['Right Back', 'Right Center Back', 'Center Back', 'Left Center Back', 'Left Back', 'Right Wing Back', 'Left Wing Back']
 
+    pos_options = team_averages.index.get_level_values(0).unique()
 
-    st.markdown("Looking at the top " + str(num_lineups) + " lineups for **" + team_option + "**.")
+    att_reduced = sorted([x for x in attack if x in pos_options])
+    mid_reduced = sorted([x for x in mid if x in pos_options])
+    def_reduced = sorted([x for x in defense if x in pos_options])
 
-    ### PULL UP LINEUPS, FIND VALUE-COUNTS, AND THEN FIND CORRESPONDING IMAGES FOR TOP 5
+    pos_form = st.form("position_widget")
+    passcol1, passcol2, passcol3 = pos_form.columns(3)
+
+    goalie_info = []
+    att_info = []
+    mid_info = []
+    def_info = []
+
+    # GOALKEEPER - goes to bottom
+    all_options = list(team_averages.loc["Goalkeeper"].index.unique())
+    label = 'Choose a Goalkeeper.'
+    goalie_name = pos_form.selectbox(label, all_options)
+    goalie_info.append(('Goalkeeper', goalie_name))
+
+    # ATTACK POS
+    passcol1.markdown("Please choose which attacking positions you would like to include.")
+    for val in att_reduced:
+        all_options = list(team_averages.loc[val].index.unique())
+        inner_options = ["None"] + all_options
+        label = 'Choose a ' + val + '.'
+        selected_name = passcol1.selectbox(label, inner_options, index = 0)
+        if selected_name != 'None':
+            att_info.append((val, selected_name))
+
+    # MID POS
+    passcol2.markdown("Please choose which midfield positions you would like to include.")
+    for val in mid_reduced:
+        all_options = list(team_averages.loc[val].index.unique())
+        inner_options = ["None"] + all_options
+        label = 'Choose a ' + val + '.'
+        selected_name = passcol2.selectbox(label, inner_options, index = 0)
+        if selected_name != 'None':
+            mid_info.append((val, selected_name))
+
+    # DEFENSE POS
+    passcol3.markdown("Please choose which defensive positions you would like to include.")
+    for val in def_reduced:
+        all_options = list(team_averages.loc[val].index.unique())
+        inner_options = ["None"] + all_options
+        label = 'Choose a ' + val + '.'
+        selected_name = passcol3.selectbox(label, inner_options, index = 0)
+        if selected_name != 'None':
+            def_info.append((val, selected_name))
+
+    pos_submitted = pos_form.form_submit_button("When you have 11 players (10 non-goalie, 1 goalie) selected, click this button.")
+
+    # ADDS AFTER SUBMIT CHECK    
+    player_indexes = goalie_info + att_info + mid_info + def_info
+    n_selected_pos = len(player_indexes)
+
+    if pos_submitted and n_selected_pos == 11:
+        #st.text(player_indexes)
+
+        # GRAPH!!!
+        pitch2 = Pitch(pitch_type = 'statsbomb', pitch_color = 'grass', line_color='white', stripe=True)
+        # specifying figure size (width, height)
+        fig4, ax4  = pitch2.draw(figsize=(16, 8))
+        for duo in player_indexes:
+            info = team_averages.loc[duo]
+            ax4.arrow(info['location_x'], info['location_y'], info['pass_length'] * np.cos(info['pass_angle']), info['pass_length'] * np.sin(info['pass_angle']), color = 'black', lw = 3)
+            ax4.scatter(info['location_x'], info['location_y'], label = duo[0], s = 100)
+            ax4.text(info['location_x'] + 1, info['location_y'] + 1, s = np.round(info['simple_pass_accuracy'], 2), color = 'yellow', fontweight = 'bold')
+        ax4.legend(bbox_to_anchor=(1.01, 1), loc='upper left')
+        st.pyplot(fig4, ax4)
+
+    elif pos_submitted and n_selected_pos != 11:
+        st.markdown('Please double check that you have selected 11 players exactly.')
+
+    # RETURN TO TEAM SELECTION
+    st.markdown(f"<a href='#{team_select}'>Try a different team?</a>", unsafe_allow_html=True)
+
 
 
