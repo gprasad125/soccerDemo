9aa1d772fed30c93c3ffc7db17b5fa141aea8042
ESR76 <esperanzasofia@gmail.com>
2022-05-15 17:53:11 -0700

    Redistributed EDA on demo site and then fixed some data (mainly data corresponding to players Quinn and Kailen Sheridan)

--- Pages/modeling.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-import numpy as np
-import pandas as pd
-
-def page():
-
-    st.markdown("## Modeling")
-    st.write("Here, we'll input our modeling.")
--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 import numpy as np
 
 # Custom imports
-from Pages import multiPage, EDA, modeling
+from Pages import multiPage, EDA, EDA2
 
 # Create an instance of the app
 app = multiPage.MultiPage()
@@ -13,7 +13,7 @@ st.markdown("# Passing and Team Success in Soccer")
 
 # Add all your application here
 app.add_page("Exploratory Data Analysis", EDA.page)
-app.add_page("Modeling", modeling.page)
+app.add_page("Build Your Own Team", EDA2.page)
 
 
 # The main app
